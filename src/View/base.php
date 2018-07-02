ea078c88b30fae5c94556b39c14a509faecd218d
Marco <marco.degennaro@edu.itspiemonte.com>
2018-07-02 22:02:34 +0200

    navbar bootstrap

--- src/View/base.php
+++ src/View/base.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>simulation-exam</title>
+	<link rel="stylesheet" type="text/css" href="../../../web/node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<div>
@@ -12,5 +13,7 @@
 		<?php require '../../Service/RouteAccess.php'; ?>
 		<?php require 'components/navbar.php'; ?>
 	</div>
+	<script type="text/javascript" src="../../../web/node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="../../../web/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
--- src/View/components/navbar.php
+++ src/View/components/navbar.php
@@ -1,10 +1,29 @@
-<ul>
-	<li><a href="../default/homepage.php">Home</a></li>
-	<li><a href="../security/register.php">Sign-up</a></li>
-	<li><a href="../security/login.php">Login</a></li>
-</ul>
-<hr>
-<div style="float: right;">
-	<?php echo isset($_SESSION['user']) ? $_SESSION['user']->getEmail().' <a href="../security/logout.php">Logout</a>' : ""; ?>
-</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="../default/homepage.php">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Activities</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <?php if(isset($_SESSION['user'])): ?>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['user']->getEmail(); ?> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="../security/logout.php">Logout</a></li>
+          </ul>
+        </li>
+    	<?php else: ?>
+    		<li><a href="../security/register.php">Register</a></li>
+        <li><a href="../security/login.php">Login</a></li>
+    	<?php endif; ?>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
